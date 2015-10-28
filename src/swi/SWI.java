3f0936277592ea2342fac434d5d52c9744b83fc2
pcmina <michalpatoprsty@gmail.com>
2015-10-28 16:11:24 +0100

    Update SWI.java

--- src/swi/SWI.java
+++ src/swi/SWI.java
@@ -15,7 +15,7 @@ public class SWI {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("kubo hra agario");
+        System.out.println("alibaba");
     }
     
 }

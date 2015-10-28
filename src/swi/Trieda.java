17947e7a1c573ffec3df303fa9cabc01ed3a0c65
xtomana <xtomana@u2-30.cpu.elf.stuba.sk>
2015-10-28 15:42:51 +0100

    prvy pokus o versioning. Vytvorenie master verzie

---
+++ src/swi/Trieda.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package swi;
+
+/**
+ *
+ * @author xtomana
+ */
+public class Trieda {
+    public Integer i;
+}

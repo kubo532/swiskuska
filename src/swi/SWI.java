9bbff4d7d84227b9deae650e68d768a0c0937592
Tomko29 <Tomko29@users.noreply.github.com>
2015-10-29 21:00:02 +0100

    Update SWI.java

--- src/swi/SWI.java
+++ src/swi/SWI.java
@@ -15,7 +15,7 @@ public class SWI {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("alibaba");
+        System.out.println("Ahoj Miško a Kubko");
     }
     
 }
